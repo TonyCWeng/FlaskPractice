e00365939202cb2f7ba4220fb08b41a184fda8eb
Tony Weng <tcweng91@gmail.com>
2019-06-06 19:51:53 -0700

    modify gitignore

--- flaskblog.py
+++ flaskblog.py
@@ -14,6 +14,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    # Backref allows us to access the user by calling post.author
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}, '{self.email}', '{self.image_file}')"
@@ -23,9 +25,10 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return return f"Post('{self.title}, '{self.date_posted}')"
+        return f"Post('{self.title}, '{self.date_posted}')"
 
 posts = [
     {
