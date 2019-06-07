85d5a5cbe82463007f1d7d1afb43a59bb6cc7ecb
Tony Weng <tcweng91@gmail.com>
2019-06-06 21:20:13 -0700

    restructure files

---
+++ blog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asfdhafhjo123123as'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# Our routes.py already imports app from this file, so in order to
+# prevent circular imports, we must import from blog after the app lines above.
+from blog import routes
