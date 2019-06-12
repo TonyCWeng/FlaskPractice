6656e854887eb095928925eb515bf70ac2c5cb81
Tony Weng <tcweng91@gmail.com>
2019-06-11 20:01:51 -0700

    implement flask-login for sessions

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asfdhafhjo123123as'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+#
+login_manager.login_view = 'login'
 
 # Our routes.py already imports app from this file, so in order to
 # prevent circular imports, we must import from blog after the app lines above.
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,14 @@
 from datetime import datetime
-from blog import db
+from blog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Inheriting from UserMixin provides 4 class properties and methods needed to
+# implement Flask-Login. (is_authenticated, is_active, is_anonymous, get_id)
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
